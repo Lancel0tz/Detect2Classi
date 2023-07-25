15bfc436057617c5901ab637bc8e6544a1fe0617
Lancel0tz <100740200+Lancel0tz@users.noreply.github.com>
2023-07-25 16:29:52 +0800

    Update detect2classi.py

--- detect2classi.py
+++ detect2classi.py
@@ -13,33 +13,32 @@ def parse_xml(xml_file):
     return class_name
 
 # Example usage
-data_folder = 'C:/Users/14869\Desktop\electronic_classification\hybrid'
-output_folder = 'C:/Users/14869\Desktop\electronic_classification\class'
+data_folder = 'path to JPG with XML folder'   
+output_folder = 'path to folder you want to store classification dataset'
 
-# 创建一个字典来存储类别对应的文件夹路径
 class_folders = {}
 
 for filename in os.listdir(data_folder):
     file_path = os.path.join(data_folder, filename)
 
-    # 筛查只读取图片文件和同名的XML文件
+    # Screening only reads image files and XML files with the same name
     if not is_xml_file(file_path):
         continue
 
     image_file = os.path.join(data_folder, f"{os.path.splitext(filename)[0]}.jpg")
 
-    # 检查同名的图片文件是否存在
+    # Check if an image file with the same name exists
     if not os.path.isfile(image_file):
         continue
 
     # Parse XML file and extract class name
     class_name = parse_xml(file_path)
 
-    # 检查该类别的文件夹是否已经创建，若未创建则创建该类别的文件夹
+    # Check whether the folder of this category has been created, if not, create a folder of this category
     if class_name not in class_folders:
         class_folder = os.path.join(output_folder, class_name)
         os.makedirs(class_folder, exist_ok=True)
         class_folders[class_name] = class_folder
 
-    # 将图片移动到对应的文件夹
+    # Move the picture to the corresponding folder
     shutil.copy(image_file, os.path.join(class_folders[class_name], os.path.basename(image_file)))
